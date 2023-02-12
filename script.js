5a385f21eff918f65449e364c9292ebcd064c3cd
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2023-02-12 19:42:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,43 @@ const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('#task-list');
 
 form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
 
-function addTask(e) {
-  e.preventDefault();
-  const task = taskInput.value;
-  if (task === '') return;
-  
-  const li = document.createElement('li');
-  li.innerText = task;
-  taskList.appendChild(li);
+loadTasks();
+
+function addTask(e) {  e.preventDefault();
+  if (!taskInput.value) return;
+
+  const task = document.createElement('li');
+  task.innerHTML = `
+    <input type="checkbox">
+    <span>${taskInput.value}</span>
+    <button class="remove-task">Remove</button>
+  `;
+
+  taskList.appendChild(task);
+
   taskInput.value = '';
+
+  saveTasks();
 }
+
+function removeTask(e) {
+  if (!e.target.classList.contains('remove-task')) return;
+
+  const task = e.target.parentElement;
+  taskList.removeChild(task);
+
+  saveTasks();
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', taskList.innerHTML);
+}
+
+function loadTasks() {
+  const tasks = localStorage.getItem('tasks');
+  if (!tasks) return;
+
+  taskList.innerHTML = tasks;
+} 
